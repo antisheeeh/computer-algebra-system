a4e9d9cda2684d1b582d83211c361cf05dca80ac
Kirill <50237109+antisheeeh@users.noreply.github.com>
2020-03-19 22:47:08 +0300

    update

--- modules/N/COM_NN_D/COM_NN_D.c
+++ modules/N/COM_NN_D/COM_NN_D.c
@@ -4,18 +4,12 @@ int compare(longNumber a, longNumber b) {
     if(a.len > b.len) return GREATER;
     else if(a.len < b.len) return LESS;
     else {
-        int i, j, p = 1;
+        int i;
 
         for(i = 0; i < a.len; ++i) {
-            if(a.num[i] / p % 10 > a.num[i] / p % 10) {
-                return GREATER;
-            } else if(a.num[i] / p % 10 < a.num[i] / p % 10) {
-                return LESS;
-            } else {
-                return EQUAL;
-            }
-
-            p *= 10;
+            if(a[i] > b[i]) return GREATER;
+            else if(a[i] < b[i]) return LESS;
+            else return EQUAL;
         }
     }
 }
