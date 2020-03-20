cebf0aa0ce0650411fc1966e258bac02f2708c4b
Kirill <50237109+antisheeeh@users.noreply.github.com>
2020-03-21 00:50:53 +0300

    changed sum function

--- modules/N/src/ADD_NN_N/ADD_NN_N.c
+++ modules/N/src/ADD_NN_N/ADD_NN_N.c
@@ -6,36 +6,30 @@
 #include "../../lib/ADD_NN_N/ADD_NN_N.h"
 
 longNumber sum(longNumber a, longNumber b) {
-    int i, carry = 0;
+    int i;
     longNumber c;
 
-    c.len = max(a.len, b.len) + 1;
-    c.num = calloc(c.len,sizeof(int));
+    c.len = max(a.len, b.len);
+    c.num = calloc(c.len + 1, sizeof(int));
 
     for(i = 0; i < c.len; ++i) {
-        if(i > a.len){
+        if(i >= a.len){
             c.num[i] += b.num[i];
-            if(c.num[i] >= BASE) {
-                c.num[i] -= BASE;
-                c.num[i+1]++;
-            }
-        } else if(i > b.len) {
+        } else if(i >= b.len) {
             c.num[i] += a.num[i];
-            if(c.num[i] >= BASE) {
-                c.num[i] -= BASE;
-                c.num[i+1]++;
-            }
         } else {
             c.num[i] += a.num[i] + b.num[i];
-            if(c.num[i] >= BASE) {
-                c.num[i] -= BASE;
-                c.num[i+1]++;
-            }
+        }
+
+        if(c.num[i] >= BASE) {
+            c.num[i] -= BASE;
+            c.num[i+1]++;
         }
     }
 
-    c.len = i;
-    c.num = realloc(c.num, sizeof(int) * c.len);
+    if(c.num[c.len] == 0) {
+        c.num = realloc(c.num, sizeof(int) * c.len);
+    }
 
     return c;
 }
