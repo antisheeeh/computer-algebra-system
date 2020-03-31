1ce95a447726c2b284586737a950acfff224d6b5
Kirill <50237109+antisheeeh@users.noreply.github.com>
2020-03-31 17:31:14 +0500

    d

--- modules/N/src/SUB_NN_N/SUB_NN_N.c
+++ modules/N/src/SUB_NN_N/SUB_NN_N.c
@@ -7,22 +7,34 @@
 
 #include "../../lib/SUB_NN_N/SUB_NN_N.h"
 #include "../../lib/COM_NN_D/COM_NN_D.h"
+#include "../../../../utils/input.h"
 
 longNumber* sub(longNumber* a, longNumber* b) {
     if(compare(a, b) == LESS) return NULL;
+
+    longNumber* c = malloc(sizeof(longNumber));
+
+    if(c == NULL) return NULL;
+
+    c->len = a->len;
+    c->num = calloc(c->len, sizeof(int));
+
+    if(c->num == NULL) return NULL;
 
     int i;
 
     for(i = 0; i < a->len; ++i) {
         if(i < b->len) {
-            a->num[i] -= b->num[i];
+            c->num[i] = a->num[i] - b->num[i];
         }
 
-        if(a->num[i] < 0) {
-            a->num[i] += BASE;
-            a->num[i+1]--;
+        if(c->num[i] < 0) {
+            c->num[i] += BASE;
+            c->num[i + 1]--;
         }
     }
 
-    return a;
+    removeTrailingZeros(c);
+
+    return c;
 }
