7622898a6ebf3ea378424904389a9344179a4528
Kirill <50237109+antisheeeh@users.noreply.github.com>
2020-03-31 22:05:08 +0500

    div_nn

--- modules/N/src/DIV_NN_Dk/DIV_NN_Dk.c
+++ modules/N/src/DIV_NN_Dk/DIV_NN_Dk.c
@@ -1,7 +1,12 @@
 #include "../../lib/DIV_NN_Dk/DIV_NN_Dk.h"
 #include "../../lib/MUL_NK_N/MUL_NK_N.h"
 #include "../../lib/COM_NN_D/COM_NN_D.h"
-#include "../../lib/SUB_NN_N/SUB_NN_N.h"
+#include "../../lib/MUL_ND_N/MUL_ND_N.h"
+#include "../../../../utils/input.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int div_nn_dk(longNumber* a, longNumber* b, int k) {
     longNumber* c = multiplyBy10k(b, k);
@@ -12,19 +17,41 @@ int div_nn_dk(longNumber* a, longNumber* b, int k) {
     } else if(res == EQUAL) {
         return 1;
     } else if(res == GREATER) {
-        int l = 0, r = c->num[0];
+        int l = 2, r = 9, m, lenC;
 
-        res = 0;
-        
-        while((compare(a, c) == GREATER) || (compare(a, c) == EQUAL)) {
-            a = sub(a, c);
-            res++;
+        char* strA = toString(a);
+        char* strC = toString(c);
+        char temp;
+
+        lenC = strlen(strC);
+        temp = strA[lenC];
+
+        strA[lenC] = '\0';
+
+        longNumber* t = parseNumber(strA);
+
+        if(compare(t, c) == LESS) {
+            strA = realloc(strA, (lenC + 2) * sizeof(char));
+            strA[lenC] = temp;
+            strA[lenC + 1] = '\0';
         }
-        
-        while (res > 9){            
-            res /= 10;
+
+        t = parseNumber(strA);
+
+        while(l <= r) {
+            m = (l + r) / 2;
+
+            res = compare(t, multiByDigit(c, m));
+
+            if(res == LESS) {
+                r = m - 1;
+            } else if(res == GREATER) {
+                l = m + 1;
+            } else if(res == EQUAL) {
+                break;
+            }
         }
-        
-        return res;
+
+        return m;
     }
 }
