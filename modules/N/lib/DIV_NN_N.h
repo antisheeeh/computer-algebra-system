f0f72d23c7c91245940160a8d08355b00bfe796f
trx <egorinanewlife@gmail.com>
2020-04-03 11:18:20 +0300

    DIV_NN_N to fix

---
+++ modules/N/lib/DIV_NN_N.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "number.h"
+#include "DIV_NN_Dk.h"
+#include "COM_NN_D.h"
+#include "SUB_NDN_N.h"
+#include "ADD_NN_N.h"
+#include "MUL_Nk_N.h"
+#include "MUL_ND_N.h"
+#include "../utils/lib/input.h"
+
+void div_nn_n(longNumberN *a, longNumberN *b);
