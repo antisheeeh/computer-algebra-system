7d82091934c2ce945db0912c1e0dac8d9211a397
Kirill <50237109+antisheeeh@users.noreply.github.com>
2020-04-14 16:30:28 +0500

    Rename MUL_Nk_N.h to MUL_NK_N.h

---
+++ modules/N/lib/MUL_NK_N.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "number.h"
+
+longNumberN* multiBy10k(longNumberN* number, int k);
--- modules/N/lib/MUL_Nk_N.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "number.h"
-
-longNumberN* multiBy10k(longNumberN* number, int k);
