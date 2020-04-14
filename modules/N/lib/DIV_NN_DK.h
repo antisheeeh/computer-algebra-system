598c40a2726cf05929c4b06a39e3c4a25db541a3
Kirill <50237109+antisheeeh@users.noreply.github.com>
2020-04-14 16:32:46 +0500

    Rename DIV_NN_Dk.h to DIV_NN_DK.h

---
+++ modules/N/lib/DIV_NN_DK.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "number.h"
+
+int div_nn_dk(longNumberN* a, longNumberN* b, int k);
--- modules/N/lib/DIV_NN_Dk.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "number.h"
-
-int div_nn_dk(longNumberN* a, longNumberN* b, int k);
